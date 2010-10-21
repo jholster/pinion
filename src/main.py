7e7d6e67ad275c5cbbeb5f85746d66b173a26bbf
Jaakko Holster <holster@iki.fi>
2010-10-21 13:44:24 +0300

    - Version history support. - Menu for resources. - Content-type guessing from url extension.

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from google.appengine.ext import db
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
+import datetime
+import os.path
 import logging
 
 
@@ -10,7 +12,36 @@ class Resource(db.Model):
 	url = db.StringProperty()
 	content_type = db.StringProperty()
 	body = db.TextProperty()
-	updated = db.DateTimeProperty(auto_now=True)
+	updated = db.DateTimeProperty()
+	latest = db.BooleanProperty(default=True)
+	
+	def put(self, *args, **kwargs):
+		previous = None
+		if self.latest == True:
+			self.updated = datetime.datetime.now()
+			if not self.content_type:
+				self.content_type = self.guess_content_type()
+			previous = Resource.get_latest(self.url)
+		super(Resource, self).put(*args, **kwargs)
+		# flag previous as non-latest after new latest succesfully saved
+		if previous:
+			previous.latest = False
+			previous.put()
+		
+	def guess_content_type(self):
+		types = { ".css": "text/css",
+		          ".js": "text/javascript",
+		          ".txt": "text/plain" }
+		suffix = os.path.splitext(self.url)[1]
+		#return ["text/html", types.get(suffix)][int(types.has_key(suffix))]
+		return types.get(suffix, "text/html")
+		
+	def get_versions(self):
+		return Resource.all().filter("url = ", self.url).order("-updated").fetch(500)
+	
+	@classmethod
+	def get_latest(cls, path):
+		return Resource.all().filter("url = ", path).filter("latest = ", True).get()
 
 
 class BaseHandler(webapp.RequestHandler):
@@ -35,30 +66,31 @@ class BaseHandler(webapp.RequestHandler):
 
 class ResourceHandler(BaseHandler):
 
-	def find_resource(self, path=None):
-		if path == None:
-			path = self.request.path
-		return Resource.all().filter("url = ", path).order("-updated").get()
-
 	def get(self, path):
-		resource = self.find_resource()
-		if self.request.get("edit", None) != None and users.is_current_user_admin():
+		if users.is_current_user_admin() and self.request.get("edit", None) != None:
+			version = self.request.get("version", None)
+			if version:
+				resource = Resource.get(version)
+			else:
+				resource = Resource.get_latest(path)
+			resource_list = Resource.all().filter("latest = ", True).order("url").fetch(500)
 			if not resource:
-				resource = Resource(url=path, content_type="text/html", body="")
-			self.render("edit.html", { "resource": resource })
-		elif resource:
-			self.response.headers["Content-Type"] = resource.content_type
-			self.response.out.write(resource.body)
+				resource = Resource(url=path, body="")
+				resource_list.append(resource)
+			self.render("edit.html", { "resource": resource,
+			                           "resource_list": resource_list })
 		else:
-			self.not_found()
+			resource = Resource.get_latest(self.request.path)
+			if resource:
+				self.response.headers["Content-Type"] = resource.content_type
+				self.response.out.write(resource.body)
+			else:
+				self.not_found()
 
 	def post(self, path):
 		if users.is_current_user_admin():
-			#resource = self.find_resource()
-			#if not resource:
 			resource = Resource()
 			resource.url = self.request.get("url", path)
-			resource.content_type = self.request.get("content_type", "text/html")
 			resource.body = self.request.get("body", "")
 			resource.put()
 			self.redirect(resource.url)
--- src/static/admin/admin.js
+++ src/static/admin/admin.js
@@ -2,24 +2,24 @@
 
 	$(function() {
 		
-		$('.admin input[name=url]').live('keyup', function() {
-			var url = $(this).val().toLowerCase().replace(/[^0-9a-z\-_\/\.]/g, '-');
-			if (url.length < 1 || url[0] != '/') url = '/' + url;
-			$(this).val(url);
-		});
-		
-		$('.admin textarea').keyup(function() {
+		$('.admin #form-edit textarea').keyup(function() {
 			if (this.scrollHeight > $(this).innerHeight())
 				$(this).height(this.scrollHeight);
 		}).trigger('keyup');
 		
-		$('.admin form').submit(function() {
+		$('.admin #form-edit').submit(function() {
 			var f = $(this);
 			$.post(f.attr('action'), f.serialize(), function() {
-				window.location.href = $('input[name=url]', f).val();
+				window.location.href = $('[name=url]', f).val();
 			});
 			return false;
 		});
+
+		$('.admin #form-edit [name=version]').change(function() {
+			$('.admin #form-version [name=version]').val($(this).val());
+			$('.admin #form-version').submit();
+		});
+		
 		
 	});
 	
